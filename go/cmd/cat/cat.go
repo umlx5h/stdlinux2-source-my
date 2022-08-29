997bca47f7db8eafa78bc39a4f49f58fbbb67bc1
umlx5h <umlx5h20@gmail.com>
2022-08-29 11:34:20 +0900

    go cat 1

---
+++ go/cmd/cat/cat.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		// if no arguments
+		fmt.Fprintf(os.Stderr, "%s: file not given\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	for _, a := range args {
+		doCat(a)
+	}
+
+}
+
+func doCat(path string) {
+	fd, err := syscall.Open(path, os.O_RDONLY, 0)
+	if err != nil {
+		die(path, err)
+	}
+
+	buf := make([]byte, 0, 256)
+
+LOOP:
+	for {
+		n, err := syscall.Read(fd, buf[:cap(buf)])
+		switch {
+		case n < 0:
+			die(path, err)
+		case n == 0:
+			break LOOP
+		default:
+			buf = buf[:n]
+		}
+		n, err = syscall.Write(int(os.Stdout.Fd()), buf)
+		if n < 0 || err != nil {
+			die(path, err)
+		}
+	}
+
+	if err := syscall.Close(fd); err != nil {
+		die(path, err)
+	}
+}
+
+func die(str string, err error) {
+	fmt.Fprintf(os.Stderr, "%s: %s", str, err)
+	os.Exit(1)
+}
---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module github.com/umlx5h/stdlinux2-source-my/go
+
+go 1.19
