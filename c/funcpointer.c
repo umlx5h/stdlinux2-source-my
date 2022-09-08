60ea92f43a693dbd375984b19c0cdf3b013bc541
umlx5h <umlx5h20@gmail.com>
2022-09-08 09:08:03 +0900

    func pointer

---
+++ c/funcpointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int plus1(int n) {
+    return n + 1;
+}
+
+int main(int argc, char *argv[]) {
+    int (*f)(int);
+    int result;
+
+    f = plus1;
+    result = f(5);
+    printf("%d\n", result);
+
+    exit(0);
+}
