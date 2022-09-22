0e97a59c84b8087c4a18bee954b85ee4fcce971c
umlx5h <umlx5h20@gmail.com>
2022-09-22 09:58:17 +0900

    httpd done

--- c/httpd.c
+++ c/httpd.c
@@ -9,9 +9,20 @@
 #include <string.h>
 #include <errno.h>
 #include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+
 
 #define MAX_REQUEST_BODY_LENGTH 4096
 #define LINE_BUF_SIZE 255
+#define BLOCK_BUF_SIZE 4096
+#define TIME_BUF_SIZE 4096
+#define HTTP_MINOR_VERSION 0
+#define SERVER_NAME "httpd"
+#define SERVER_VERSION "1.0"
 
 static void log_exit(char *fmt, ...) {
     va_list ap;
@@ -238,8 +249,155 @@ static void free_request(struct HTTPRequest *req) {
     free(req);
 }
 
-static void respond_to(struct HTTPRequest *req, FILE *out, char *docroot) {
+static char *build_fspath(char *docroot, char *urlpath) {
+    char *path;
+    //                            スラッシュ              NULL文字
+    path = xmalloc(strlen(docroot) + 1 + strlen(urlpath) + 1);
+    sprintf(path, "%s/%s", docroot, urlpath);
 
+    return path;
+}
+
+
+struct FileInfo {
+    char *path;
+    long size;
+    int ok;
+};
+
+static void free_fileinfo(struct FileInfo *info) {
+    free(info->path);
+    free(info);
+}
+
+static struct FileInfo *get_fileinfo(char *docroot, char *urlpath) {
+    struct FileInfo *info;
+    struct stat st;
+
+    info = xmalloc(sizeof(struct FileInfo));
+    info->path = build_fspath(docroot, urlpath);
+    info->ok = 0;
+    if (lstat(info->path, &st) < 0) return info;
+    // regular fileか確認
+    if (!S_ISREG(st.st_mode)) return info;
+    info->ok = 1;
+    info->size = st.st_size;
+    return info;
+}
+
+static char *guess_content_type(struct FileInfo *info) {
+    return "text/plain";
+}
+
+static void output_common_header_fields(struct HTTPRequest *req, FILE *out, char *status) {
+    time_t t;
+    struct tm *tm;
+    char buf[TIME_BUF_SIZE];
+
+    t = time(NULL);
+    tm = gmtime(&t);
+    if (!tm) log_exit("gmtime() failed: %s", strerror(errno));
+    strftime(buf, TIME_BUF_SIZE, "%a, %d %b %Y %H:%M:%S GMT", tm);
+    fprintf(out, "HTTP/1.%d %s\r\n", HTTP_MINOR_VERSION, status);
+    fprintf(out, "Date: %s\r\n", buf);
+    fprintf(out, "Server: %s/%s\r\n", SERVER_NAME, SERVER_VERSION);
+    fprintf(out, "Connection: close\r\n");
+}
+
+static void method_not_allowed(struct HTTPRequest *req, FILE *out) {
+    output_common_header_fields(req, out, "405 Method Not Allowed");
+    fprintf(out, "Content-Type: text/html\r\n");
+    fprintf(out, "\r\n");
+    fprintf(out, "<html>\r\n");
+    fprintf(out, "<header>\r\n");
+    fprintf(out, "<title>405 Method Not Allowed</title>\r\n");
+    fprintf(out, "<header>\r\n");
+    fprintf(out, "<body>\r\n");
+    fprintf(out, "<p>The request method %s is not allowed</p>\r\n", req->method);
+    fprintf(out, "</body>\r\n");
+    fprintf(out, "</html>\r\n");
+    fflush(out);
+}
+
+static void not_implemented(struct HTTPRequest *req, FILE *out) {
+    output_common_header_fields(req, out, "501 Not Implemented");
+    fprintf(out, "Content-Type: text/html\r\n");
+    fprintf(out, "\r\n");
+    fprintf(out, "<html>\r\n");
+    fprintf(out, "<header>\r\n");
+    fprintf(out, "<title>501 Not Implemented</title>\r\n");
+    fprintf(out, "<header>\r\n");
+    fprintf(out, "<body>\r\n");
+    fprintf(out, "<p>The request method %s is not implemented</p>\r\n", req->method);
+    fprintf(out, "</body>\r\n");
+    fprintf(out, "</html>\r\n");
+    fflush(out);
+}
+
+static void not_found(struct HTTPRequest *req, FILE *out) {
+    output_common_header_fields(req, out, "404 Not Found");
+    fprintf(out, "Content-Type: text/html\r\n");
+    fprintf(out, "\r\n");
+    if (strcmp(req->method, "HEAD") != 0) {
+        fprintf(out, "<html>\r\n");
+        fprintf(out, "<header><title>Not Found</title><header>\r\n");
+        fprintf(out, "<body><p>File not found</p></body>\r\n");
+        fprintf(out, "</html>\r\n");
+    }
+    fflush(out);
+}
+
+static void do_file_response(struct HTTPRequest *req, FILE *out, char *docroot) {
+    struct FileInfo *info;
+
+    info = get_fileinfo(docroot, req->path);
+    if (!info->ok) {
+        free_fileinfo(info);
+        not_found(req, out);
+        return;
+    }
+    output_common_header_fields(req, out, "200 OK");
+    fprintf(out, "Content-Length: %ld\r\n", info->size);
+    fprintf(out, "Content-Type: %s\r\n", guess_content_type(info));
+    fprintf(out, "\r\n");
+    // if GET
+    if (strcmp(req->method, "HEAD") != 0) {
+        int fd;
+        char buf[BLOCK_BUF_SIZE];
+        ssize_t n;
+
+        fd = open(info->path, O_RDONLY);
+
+        if (fd < 0)
+            log_exit("failed to open %s: %s", info->path, strerror(errno));
+        for (;;) {
+            n = read(fd, buf, BLOCK_BUF_SIZE);
+            if (n < 0)
+                log_exit("failed to read %s: %s", info->path, strerror(errno));
+            
+            // 最後まで読んだ場合
+            if (n == 0)
+                break;
+            // 呼んだ分だけソケットに対して書き込み
+            if (fwrite(buf, 1, n, out) < n)
+                log_exit("failed to write to socket: %s", strerror(errno));
+        } 
+        close(fd);
+    }
+
+    fflush(out);
+    free_fileinfo(info);
+}
+
+static void respond_to(struct HTTPRequest *req, FILE *out, char *docroot) {
+    if (strcmp(req->method, "GET") == 0)
+        do_file_response(req, out, docroot);
+    else if (strcmp(req->method, "HEAD") == 0)
+        do_file_response(req, out, docroot);
+    else if (strcmp(req->method, "POST") == 0)
+        method_not_allowed(req, out);
+    else
+        not_implemented(req, out);
 }
 
 static void service(FILE *in, FILE *out, char *docroot) {
@@ -250,7 +408,7 @@ static void service(FILE *in, FILE *out, char *docroot) {
     free_request(req);
 }
 
-int main(int argc, char *argv[]) {
+void debug() {
     struct HTTPRequest *req;
     FILE *file;
     file = fopen("testdata/get_withbody.txt", "r");
@@ -269,11 +427,12 @@ int main(int argc, char *argv[]) {
 
     if (req->length > 0) {
         printf("request body: %s\n", req->body);
-
     }
+}
 
-
-    exit(0);
+int main(int argc, char *argv[]) {
+    // debug();
+    // exit(0);
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <docroot>\n", argv[0]);
