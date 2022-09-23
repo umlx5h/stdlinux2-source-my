418f2f7daa888e3137fc7f40af35577250e9e778
umlx5h <umlx5h20@gmail.com>
2022-09-23 20:22:25 +0900

    httpd2 first

--- c/httpd2.c
+++ c/httpd2.c
@@ -15,16 +15,23 @@
 #include <fcntl.h>
 #include <time.h>
 
+#include <getopt.h>
+#include <syslog.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 #define MAX_REQUEST_BODY_LENGTH 4096
 #define LINE_BUF_SIZE 255
 #define BLOCK_BUF_SIZE 4096
 #define TIME_BUF_SIZE 4096
 #define HTTP_MINOR_VERSION 0
-#define SERVER_NAME "httpd"
+#define SERVER_NAME "httpd2"
 #define SERVER_VERSION "1.0"
 
-static void log_exit(char *fmt, ...) {
+#define USAGE "Usage: %s [--port=n] [--chroot --user=u --group=g] <docroot>\n"
+#define MAX_BACKLOG 5
+
+static void log_exit(const char *fmt, ...) {
     va_list ap;
 
     va_start(ap, fmt);
@@ -56,8 +63,26 @@ static void signal_exit(int sig) {
     log_exit("exit by signal %d", sig);
 }
 
+static void noop_handler(int sig) {
+    ;
+}
+
+// 子プロセスがゾンビになるのを防止する、wait()を呼ばないと宣言する
+static void detach_children(void) {
+    struct sigaction act;
+
+    act.sa_handler = noop_handler;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = SA_RESTART | SA_NOCLDWAIT;
+    if (sigaction(SIGCHLD, &act, NULL) < 0) {
+        log_exit("sigaction(2) failed: %s", strerror(errno));
+    }
+}
+
+
 static void install_signal_handlers(void) {
     trap_signal(SIGPIPE, signal_exit);
+    detach_children();
 }
 
 struct HTTPHeaderField {
@@ -430,17 +455,151 @@ void debug() {
     }
 }
 
-int main(int argc, char *argv[]) {
-    // debug();
-    // exit(0);
+static void setup_environment(char *docroot, char *user, char *group) {
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <docroot>\n", argv[0]);
+}
+
+static void become_daemon() {
+
+}
+
+// socket, bind, listenを実行してソケットを返す
+static int listen_socket(char *port) {
+    struct addrinfo hints, *res, *ai;
+    int err;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET; // IPv4接続
+    hints.ai_socktype = SOCK_STREAM; // TCP (コネクション型)
+    hints.ai_flags = AI_PASSIVE; // サーバー側はPASSIVEに設定
+    if ((err = getaddrinfo(NULL, port, &hints, &res)) != 0) {
+        log_exit(gai_strerror(err));
+    }
+
+    for (ai = res; ai; ai = ai->ai_next) {
+        int sock; 
+
+        // 1. socket(2) で ソケットを作成
+        sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+        if (sock < 0) continue;
+
+        // 2. bind(2) で 特定ポートにソケットをバインドする
+        if (bind(sock, ai->ai_addr, ai->ai_addrlen) < 0) {
+            close(sock);
+            continue;
+        }
+
+        // 3. listen(2) で ソケットをパッシブソケットとして設定する (接続要求を受けつけれるようにする)
+        if (listen(sock, MAX_BACKLOG) < 0) {
+            close(sock);
+            continue;
+        }
+
+        freeaddrinfo(res);
+        return sock;
+    }
+
+    // not reached here
+    log_exit("failed to listen socket");
+
+    return -1; 
+}
+
+// accept(2)をループする関数
+static void server_main(int server_fd, char *docroot) {
+    for (;;) {
+        struct sockaddr_storage addr;
+        socklen_t addrlen = sizeof addr;
+        int sock;
+        int pid;
+
+        // TODO: 事前にforkしておいてそれぞれのプロセスでacceptを呼び出せばプリフォークモデルとなる
+        // これは並行モデル (concurrency model)
+        // accpetしたらすぐにforkして子プロセスがクライアントと通信する
+        sock = accept(server_fd, (struct sockaddr*)&addr, &addrlen);
+        if (sock < 0) log_exit("accept(2) failed: %s", strerror(errno));
+        // リクエスト解析&レスポンスを返す処理は子プロセスに任せる
+        pid = fork();
+        if (pid < 0) exit(3);
+        if (pid == 0) { // 子プロセス
+            // 読み込み書き込み両方とも同じソケットを使う (accept(2)でもらったソケット)
+            FILE *inf = fdopen(sock, "r");
+            FILE *outf = fdopen(sock, "w");
+
+            service(inf, outf, docroot);
+            exit(0);
+        }
+
+        // 親プロセスでは使っていないためcloseしないといけない
+        // closeすることで参照カウントを1つ減らすことになる(fdが差すポインタを子プロセスにコピーしているような挙動になっている)
+        close(sock);
+    }
+}
+
+static int debug_mode = 0;
+static struct option longopts[] = {
+    {"debug", no_argument, &debug_mode, 1},
+    {"chroot", no_argument, NULL, 'c'},
+    {"user", required_argument, NULL, 'u'},
+    {"group", required_argument, NULL, 'g'},
+    {"port", required_argument, NULL, 'p'},
+    {"help", no_argument, NULL, 'h'},
+    {0, 0, 0, 0}
+};
+
+int main(int argc, char *argv[]) {
+    int server_fd;
+    char *port = NULL;
+    char *docroot;
+    int do_chroot = 0;
+    char *user = NULL;
+    char *group = NULL;
+    int opt;
+    
+    while ((opt = getopt_long(argc, argv, "", longopts, NULL)) != -1) {
+        switch (opt) {
+        case 0:
+            break;
+        case 'c':
+            do_chroot = 1;
+            break;
+        case 'u':
+            user = optarg;
+            break;
+        case 'g':
+            group = optarg;
+            break;
+        case 'p':
+            port = optarg;
+            break;
+        case 'h':
+            fprintf(stdout, USAGE, argv[0]);
+            exit(0);
+        case '?':
+            fprintf(stderr, USAGE, argv[0]);
+            exit(1);
+        }
+    }
+    if (optind != argc - 1) {
+        fprintf(stderr, USAGE, argv[0]);
         exit(1);
+    } 
+    docroot = argv[optind];
+
+    if (do_chroot) {
+        setup_environment(docroot, user, group);
+        docroot = "";
     }
 
     install_signal_handlers();
-    service(stdin, stdout, argv[1]);
+    server_fd = listen_socket(port);
+
+    if (!debug_mode) {
+        openlog(SERVER_NAME, LOG_PID|LOG_NDELAY, LOG_DAEMON);
+        become_daemon();
+    }
+
+    server_main(server_fd, docroot);
     exit(0);
 }
 
